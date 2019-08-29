947dcea745eb98f7804d493867f567baae446bd3
Simon Dodsley <simon@purestorage.com>
2019-08-29 11:39:07 -0400

    Update purefa_info.py

--- collections/ansible_collections/purestorage/flasharray/plugins/modules/purefa_info.py
+++ collections/ansible_collections/purestorage/flasharray/plugins/modules/purefa_info.py
@@ -644,17 +644,25 @@ def generate_vol_dict(array):
         vvols = array.list_volumes(protocol_endpoint=True)
         for vvol in range(0, len(vvols)):
             volume = vvols[vvol]['name']
-            volume_info[volume]['source'] = vvols[vvol]['source']
-            volume_info[volume]['serial'] = vvols[vvol]['serial']
+            volume_info[volume] = {
+                'source': vvols[vvol]['source'],
+                'serial': vvols[vvol]['serial'],
+                'hosts': []
+            }
+            if P53_API_VERSION in array._list_available_rest_versions():
+                pe_e2ees = array.list_volumes(protocol_endpoint=True, host_encryption_key=True)
+                for pe_e2ee in range(0, len(pe_e2ees)):
+                    volume = pe_e2ees[pe_e2ee]['name']
+                    volume_info[volume]['host_encryption_key_status'] = pe_e2ees[pe_e2ee]['host_encryption_key_status']
         if P53_API_VERSION in array._list_available_rest_versions():
             e2ees = array.list_volumes(host_encryption_key=True)
             for e2ee in range(0, len(e2ees)):
                 volume = e2ees[e2ee]['name']
-                volume_info[volume]['host_encryption_key_status'] = e2ees[e2ee]['host_encryption_key_status'],
+                volume_info[volume]['host_encryption_key_status'] = e2ees[e2ee]['host_encryption_key_status']
     cvols = array.list_volumes(connect=True)
     for cvol in range(0, len(cvols)):
         volume = cvols[cvol]['name']
-        voldict = [cvols[cvol]['host'], cvols[cvol]['lun']]
+        voldict = {'host': cvols[cvol]['host'], 'lun': cvols[cvol]['lun']}
         volume_info[volume]['hosts'].append(voldict)
     return volume_info
 
