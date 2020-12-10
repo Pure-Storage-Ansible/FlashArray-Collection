a37b64b260cad457af11fbb90bf703f05fd4230c
Simon Dodsley <simon@purestorage.com>
2020-12-10 11:31:56 -0500

    Fix DNS idempotency

--- collections/ansible_collections/purestorage/flasharray/plugins/modules/purefa_dns.py
+++ collections/ansible_collections/purestorage/flasharray/plugins/modules/purefa_dns.py
@@ -94,12 +94,14 @@ def create_dns(module, array):
     """Set DNS settings"""
     changed = True
     if not module.check_mode:
+        changed = False
         current_dns = array.get_dns()
         if current_dns['domain'] != module.params['domain'] or \
            sorted(module.params['nameservers']) != sorted(current_dns['nameservers']):
             try:
                 array.set_dns(domain=module.params['domain'],
                               nameservers=module.params['nameservers'][0:3])
+                changed = True
             except Exception:
                 module.fail_json(msg='Set DNS settings failed: Check configuration')
     module.exit_json(changed=changed)
