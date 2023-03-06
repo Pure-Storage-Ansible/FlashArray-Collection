4fe91aead1b6fa2ce03b658a64deb3108c8eace3
Simon Dodsley <simon@purestorage.com>
2023-03-06 11:10:30 -0500

    Update purefa_pgsnap.py

--- plugins/modules/purefa_pgsnap.py
+++ plugins/modules/purefa_pgsnap.py
@@ -143,7 +143,7 @@ EXAMPLES = r"""
     suffix: force
     now: true
     apply_retention: true
-    remote: True
+    remote: true
     fa_url: 10.10.10.2
     api_token: e31060a7-21fc-e277-6240-25983c6c4592
 
