fe347bb9a7a6d2f598b5a33b19ab6284a34fbeac
Simon Dodsley <simon@purestorage.com>
2024-08-19 10:14:22 -0400

    Lint fix

--- plugins/modules/purefa_dsrole.py
+++ plugins/modules/purefa_dsrole.py
@@ -194,7 +194,7 @@ def main():
     state = module.params["state"]
     array = get_array(module)
     api_version = array.get_rest_version()
-        if LooseVersion(MIN_DSROLE_API_VERSION) <= LooseVersion(api_version):
+    if LooseVersion(MIN_DSROLE_API_VERSION) <= LooseVersion(api_version):
         module.fail_json(
             msg="This module requires Purity//FA 6.6.3 and higher"
         )
