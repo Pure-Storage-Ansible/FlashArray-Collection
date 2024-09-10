2c0874fffbdb18b753d3ee137dbbca7460b00ebb
Simon Dodsley <simon@purestorage.com>
2024-09-10 14:35:09 -0400

    Fix doc typo

--- plugins/modules/purefa_export.py
+++ plugins/modules/purefa_export.py
@@ -65,7 +65,7 @@ EXAMPLES = r"""
     filesystem: bar
     directory: foo
     nfs_policy: nfs-example
-    smb_polict: smb-example
+    smb_policy: smb-example
     fa_url: 10.10.10.2
     api_token: e31060a7-21fc-e277-6240-25983c6c4592
 
