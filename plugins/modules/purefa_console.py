1f12c7f818d024f4c27541f03d56958cc5484fd6
Simon Dodsley <simon@purestorage.com>
2024-09-19 10:32:53 -0400

    Update purefa_console.py

--- plugins/modules/purefa_console.py
+++ plugins/modules/purefa_console.py
@@ -38,7 +38,7 @@ extends_documentation_fragment:
 EXAMPLES = r"""
 - name: Enable Console Lockout
   purestorage.flasharray.purefa_console:
-    state: disable
+    state: enable
     fa_url: 10.10.10.2
     api_token: e31060a7-21fc-e277-6240-25983c6c4592
 
