606db1a5d06ea02989836298ea833afebb9c745d
Simon Dodsley <simon@purestorage.com>
2024-10-16 12:39:26 -0400

    Fix SMTP config deletion

--- plugins/modules/purefa_smtp.py
+++ plugins/modules/purefa_smtp.py
@@ -84,7 +84,7 @@ def delete_smtp(module, array):
     changed = True
     if not module.check_mode:
         try:
-            array.set_smtp(sender_domain="", user_name="", password="", relay_host="")
+            array.set_smtp(sender_domain="None", user_name="", password="", relay_host="")
         except Exception:
             module.fail_json(msg="Delete SMTP settigs failed")
     module.exit_json(changed=changed)
