8dba16643fbdd8fccbc82aa534ca3afa610c3d3e
Simon Dodsley <simon@purestorage.com>
2025-03-19 16:52:11 -0400

    Fix issue with adding/removing existing volume from a PG

--- plugins/modules/purefa_volume.py
+++ plugins/modules/purefa_volume.py
@@ -1334,6 +1334,45 @@ def update_volume(module, array):
                             module.params["name"], prio_res.errors[0].message
                         )
                     )
+    if module.params["add_to_pgs"]:
+        pgs_now = []
+        if LooseVersion(CONTEXT_API_VERSION) <= LooseVersion(api_version):
+            current_pgs = list(
+                array.get_protection_groups_volumes(
+                    context_names=[module.params["context"]],
+                    members=[FixedReference(name=module.params["name"])],
+                ).items
+            )
+        else:
+            current_pgs = list(
+                array.get_protection_groups_volumes(
+                    members=[FixedReference(name=module.params["name"])]
+                ).items
+            )
+        for current_pg in range(0, len(current_pgs)):
+            pgs_now.append(current_pgs[current_pg].group.name)
+        new_pgs = list(filter(lambda x: x not in pgs_now, module.params["add_to_pgs"]))
+        if new_pgs:
+            changed = True
+            if LooseVersion(CONTEXT_API_VERSION) <= LooseVersion(api_version):
+                res = array.post_volumes_protection_groups(
+                    member_names=[module.params["name"]],
+                    group_names=new_pgs,
+                    context_names=[module.params["context"]],
+                )
+            else:
+                res = array.post_volumes_protection_groups(
+                    member_names=[module.params["name"]],
+                    group_names=new_pgs,
+                )
+            if res.status_code != 200:
+                module.fail_json(
+                    msg="Failed to add volume {0} to new PGs {1}: Error: {2}".format(
+                        module.params["name"],
+                        new_pgs,
+                        res.errors[0].message,
+                    )
+                )
     module.exit_json(
         changed=changed, volume=_volfact(module, array, module.params["name"])
     )
@@ -1613,39 +1652,79 @@ def move_volume(module, array):
 def delete_volume(module, array):
     """Delete Volume"""
     api_version = array.get_rest_version()
-    changed = True
-    if not module.check_mode:
+    changed = False
+    if module.params["add_to_pgs"]:
+        pgs_now = []
         if LooseVersion(CONTEXT_API_VERSION) <= LooseVersion(api_version):
-            res = array.patch_volumes(
-                names=[module.params["name"]],
-                volume=VolumePatch(destroyed=True),
-                context_names=[module.params["context"]],
+            current_pgs = list(
+                array.get_protection_groups_volumes(
+                    context_names=[module.params["context"]],
+                    members=[FixedReference(name=module.params["name"])],
+                ).items
             )
         else:
-            res = array.patch_volumes(
-                names=[module.params["name"]], volume=VolumePatch(destroyed=True)
+            current_pgs = list(
+                array.get_protection_groups_volumes(
+                    members=[FixedReference(name=module.params["name"])]
+                ).items
             )
-        if res.status_code == 200 and module.params["eradicate"]:
+        for current_pg in range(0, len(current_pgs)):
+            pgs_now.append(current_pgs[current_pg].group.name)
+        old_pgs = list(filter(lambda x: x in module.params["add_to_pgs"], pgs_now))
+        if old_pgs:
+            changed = True
+            if not module.check_mode:
+                if LooseVersion(CONTEXT_API_VERSION) <= LooseVersion(api_version):
+                    res = array.delete_volumes_protection_groups(
+                        member_names=[module.params["name"]],
+                        group_names=old_pgs,
+                        context_names=[module.params["context"]],
+                    )
+                else:
+                    res = array.delete_volumes_protection_groups(
+                        member_names=[module.params["name"]],
+                        group_names=old_pgs,
+                    )
+                if res.status_code != 200:
+                    module.fail_json(
+                        msg="Failed to remove volume {0} from PGs {1}: Error: {2}".format(
+                            module.params["name"], old_pgs, res.errors[0].message
+                        )
+                    )
+    else:
+        changed = True
+        if not module.check_mode:
             if LooseVersion(CONTEXT_API_VERSION) <= LooseVersion(api_version):
-                res = array.delete_volumes(
+                res = array.patch_volumes(
                     names=[module.params["name"]],
+                    volume=VolumePatch(destroyed=True),
                     context_names=[module.params["context"]],
                 )
             else:
-                res = array.delete_volumes(names=[module.params["name"]])
-            if res.status_code != 200:
+                res = array.patch_volumes(
+                    names=[module.params["name"]], volume=VolumePatch(destroyed=True)
+                )
+            if res.status_code == 200 and module.params["eradicate"]:
+                if LooseVersion(CONTEXT_API_VERSION) <= LooseVersion(api_version):
+                    res = array.delete_volumes(
+                        names=[module.params["name"]],
+                        context_names=[module.params["context"]],
+                    )
+                else:
+                    res = array.delete_volumes(names=[module.params["name"]])
+                if res.status_code != 200:
+                    module.fail_json(
+                        msg="Eradicate volume {0} failed. Error: {1}".format(
+                            module.params["name"], res.errors[0].message
+                        )
+                    )
+                module.exit_json(changed=changed, volume=[])
+            else:
                 module.fail_json(
-                    msg="Eradicate volume {0} failed. Error: {1}".format(
+                    msg="Delete volume {0} failed. Error: {1}".format(
                         module.params["name"], res.errors[0].message
                     )
                 )
-            module.exit_json(changed=changed, volume=[])
-        else:
-            module.fail_json(
-                msg="Delete volume {0} failed. Error: {1}".format(
-                    module.params["name"], res.errors[0].message
-                )
-            )
     module.exit_json(
         changed=changed, volume=_volfact(module, array, module.params["name"])
     )
@@ -1850,7 +1929,13 @@ def main():
         elif (
             state == "present"
             and volume
-            and (size or bw_qos or iops_qos or module.params["promotion_state"])
+            and (
+                size
+                or bw_qos
+                or iops_qos
+                or module.params["promotion_state"]
+                or module.params["add_to_pgs"]
+            )
         ):
             update_volume(module, array)
         elif state == "present" and not volume and module.params["move"]:
