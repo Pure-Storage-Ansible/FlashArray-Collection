6f37a0b190effc792172cf5aea390f9eb742cc1e
Simon Dodsley <simon@purestorage.com>
2026-02-02 17:31:59 -0500

    Fix typo in inventory collection example

--- plugins/modules/purefa_inventory.py
+++ plugins/modules/purefa_inventory.py
@@ -28,7 +28,7 @@ extends_documentation_fragment:
 """
 
 EXAMPLES = r"""
-- name: collect FlashArray invenroty
+- name: collect FlashArray inventory
   purestorage.flasharray.purefa_inventory:
     fa_url: 10.10.10.2
     api_token: e31060a7-21fc-e277-6240-25983c6c4592
